b00bc8dae865fcf31f1b57109afa56bd2988df34
duckale0976 <duckale0976@gmail.com>
2025-12-09 00:58:49 +0700

    Create config.js
    Chứa cấu hình Firebase & Dữ liệu tĩnh

---
+++ js/config.js
@@ -0,0 +1,39 @@
+// js/config.js
+export const CONFIG = {
+    firebase: {
+        apiKey: "AIzaSyAcfs8LFgPeAkGyaSkmJeECoRBEwtfRv8E",
+        authDomain: "minhduckale1.firebaseapp.com",
+        databaseURL: "https://minhduckale1-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "minhduckale1",
+        storageBucket: "minhduckale1.firebasestorage.app",
+        messagingSenderId: "479002042854",
+        appId: "1:479002042854:web:c9e4b614ad48fefd10f627"
+    },
+    folders: {
+        "capcuu": { name: "1. Hồi sức Cấp cứu", icon: "fa-truck-medical", color: "text-red-500" },
+        "noikhoa": { name: "2. Nội khoa", icon: "fa-user-doctor", color: "text-blue-500" },
+        "ngoaikhoa": { name: "3. Ngoại khoa", icon: "fa-scalpel", color: "text-green-600" },
+        "san": { name: "4. Sản phụ khoa", icon: "fa-person-pregnant", color: "text-pink-500" },
+        "nhi": { name: "5. Nhi khoa", icon: "fa-baby", color: "text-orange-500" },
+        "canlamsang": { name: "6. Cận lâm sàng", icon: "fa-microscope", color: "text-purple-500" },
+        "khac": { name: "7. Tài liệu khác", icon: "fa-folder-open", color: "text-slate-500" }
+    },
+    playlist: [
+        {t:"Lofi Study 1", a:"Lofi Girl", u:"https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3?filename=lofi-study-112191.mp3"},
+        {t:"Rainy Night", a:"Relaxing", u:"https://cdn.pixabay.com/download/audio/2022/03/10/audio_5b8220a28f.mp3?filename=piano-moment-11153.mp3"},
+        {t:"Deep Focus", a:"Mindset", u:"https://cdn.pixabay.com/download/audio/2021/11/01/audio_6d6787920b.mp3?filename=chill-abstract-intention-12099.mp3"},
+        {t:"Coffee Shop", a:"Ambience", u:"https://cdn.pixabay.com/download/audio/2022/01/18/audio_d0a13f69d2.mp3?filename=loneliness-1476.mp3"},
+        {t:"Sunday Morning", a:"Happy", u:"https://cdn.pixabay.com/download/audio/2020/05/18/audio_59146522e0.mp3?filename=smooth-waters-11218.mp3"},
+        {t:"Late Night Vibe", a:"Lofi", u:"https://cdn.pixabay.com/download/audio/2022/10/25/audio_1d9e25d204.mp3?filename=lofi-chill-medium-version-126435.mp3"},
+        {t:"Coding Mode", a:"Focus", u:"https://cdn.pixabay.com/download/audio/2022/02/07/audio_3332560882.mp3?filename=soft-beat-636.mp3"},
+        {t:"Tokyo Rain", a:"Atmosphere", u:"https://cdn.pixabay.com/download/audio/2022/03/24/audio_331b262846.mp3?filename=lost-in-thought-113264.mp3"},
+        {t:"Jazzy Beats", a:"Groove", u:"https://cdn.pixabay.com/download/audio/2022/05/05/audio_1385f09623.mp3?filename=jazz-hop-11442.mp3"},
+        {t:"Sleepy Cat", a:"Relax", u:"https://cdn.pixabay.com/download/audio/2021/09/06/audio_3702159043.mp3?filename=sweet-dreams-9408.mp3"}
+    ],
+    ambience: {
+        rain: "https://actions.google.com/sounds/v1/weather/rain_heavy_loud.ogg",
+        fire: "https://actions.google.com/sounds/v1/ambiences/fireplace.ogg",
+        forest: "https://actions.google.com/sounds/v1/nature/forest_atmosphere.ogg", 
+        cafe: "https://actions.google.com/sounds/v1/ambiences/coffee_shop.ogg"
+    }
+};
